a1b6fb66d302cfa2810b1eb28bd36c52a93d7f5e
duycoem <85142827+duycoem@users.noreply.github.com>
2023-02-04 10:44:32 +0700

    Khởi tạo dự án

---
+++ QuizIT/QuizIT/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QuizIT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
